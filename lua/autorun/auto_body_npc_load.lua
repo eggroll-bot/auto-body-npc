acdd621808ecb164861a81d6e45ffee04f5671d7
Brian Mak <eggrollbot@gmail.com>
2019-12-24 00:13:05 -0800

    Removed an accidental include
    Removed an accidental include on the server of a clientside file.

--- lua/autorun/auto_body_npc_load.lua
+++ lua/autorun/auto_body_npc_load.lua
@@ -4,7 +4,6 @@ AutoBodyNPC.Config = { }
 if SERVER then
 	AddCSLuaFile( "auto_body_npc/cl_panels.lua" )
 	AddCSLuaFile( "auto_body_npc/sh_config.lua" )
-	include( "auto_body_npc/cl_panels.lua" )
 	include( "auto_body_npc/sh_config.lua" )
 	resource.AddFile( "resource/fonts/roboto_medium.ttf" )
 elseif CLIENT then
