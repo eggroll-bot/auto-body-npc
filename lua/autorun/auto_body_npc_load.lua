640b11dbe3e87fd468f070c1b77fa03aff19b464
Brian Mak <eggrollbot@gmail.com>
2019-12-24 12:00:02 -0800

    Added the resource.AddFile for the logo
    Added the resource.AddFile call for materials/auto_body_npc/logo.png.

--- lua/autorun/auto_body_npc_load.lua
+++ lua/autorun/auto_body_npc_load.lua
@@ -7,6 +7,7 @@ if SERVER then
 	AddCSLuaFile( "auto_body_npc/sh_config.lua" )
 	include( "auto_body_npc/sh_config.lua" )
 	resource.AddFile( "resource/fonts/roboto_medium.ttf" )
+	resource.AddFile( "materials/auto_body_npc/logo.png" )
 elseif CLIENT then
 	include( "auto_body_npc/cl_fonts.lua" )
 	include( "auto_body_npc/cl_panels.lua" )
