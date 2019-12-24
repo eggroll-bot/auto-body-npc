2e4e3e679f6ae5a2cf209e5aa4619bd61627ef46
Brian Mak <eggrollbot@gmail.com>
2019-12-24 15:34:03 -0800

    Renamed sv_hooks.lua to sh_hooks.lua and included it in both realms
    Renamed sv_hooks.lua to sh_hooks.lua and included it in both the server and the client realm.

---
+++ lua/auto_body_npc/sh_hooks.lua
@@ -0,0 +1,14 @@
+hook.Add( "VC_CD_spawnedVehicle", "AutoBodyNPC_SpawnVehicle", function( ply, vehicle, test_drive ) -- We're going to interact as little as we can with VCMod because their API is terrible and most of the documentation sucks.
+	if not test_drive then
+		vehicle:SetNWEntity( "VCModOwner", ply )
+		-- set bodygroups, color, skin, underglow, and engine upgrades, if any.
+	end
+end )
+
+hook.Add( "CanProperty", "AutoBodyNPC_CanCustomizeProperties", function( _, property, ent )
+	if IsValid( ent:GetNWEntity( "VCModOwner" ) ) and ( property == "skin" or property == "bodygroups" ) then -- Vehicle is from VCMod.
+		notification.AddLegacy( "You must use the auto body shop to customize your vehicle.", NOTIFY_ERROR, 3 )
+
+		return false
+	end
+end )
