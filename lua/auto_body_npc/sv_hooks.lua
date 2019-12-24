0f1bc9a09024335c1cf60c1a0669df10c8133b4a
Brian Mak <eggrollbot@gmail.com>
2019-12-24 15:46:05 -0800

    Removed cl_hooks.lua and removed the includes for it
    Removed cl_hooks.lua and removed the includes for it.

--- lua/auto_body_npc/sv_hooks.lua
+++ lua/auto_body_npc/sv_hooks.lua
@@ -7,8 +7,8 @@ end )
 
 hook.Add( "CanProperty", "AutoBodyNPC_CanCustomizeProperties", function( _, property, ent )
 	if IsValid( ent:GetNWEntity( "VCModOwner" ) ) and ( property == "skin" or property == "bodygroups" ) then -- Vehicle is from VCMod.
-		notification.AddLegacy( "You must use the auto body shop to customize your vehicle.", NOTIFY_ERROR, 3 )
-
 		return false
 	end
 end )
+
+hook.Remove( "CanProperty", "AutoBodyNPC_CanCustomizeProperties" )
