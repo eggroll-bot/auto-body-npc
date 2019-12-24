97a2b37f889df7c403e0f4973f6f07be793c6c56
Brian Mak <eggrollbot@gmail.com>
2019-12-23 22:40:01 -0800

    Added Roboto Medium and included it
    Added the Roboto Medium font for use with the customs NPC and resource.AddFile'd it.

--- lua/autorun/rockford_customs_npc_load.lua
+++ lua/autorun/rockford_customs_npc_load.lua
@@ -6,6 +6,7 @@ if SERVER then
 	AddCSLuaFile( "rockford_customs_npc/sh_config.lua" )
 	include( "rockford_customs_npc/cl_panels.lua" )
 	include( "rockford_customs_npc/sh_config.lua" )
+	resource.AddFile( "resource/fonts/roboto_medium.ttf" )
 elseif CLIENT then
 	include( "rockford_customs_npc/cl_panels.lua" )
 	include( "rockford_customs_npc/sh_config.lua" )
