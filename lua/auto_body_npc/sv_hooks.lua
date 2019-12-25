99e64687ea3dfaf094f2c6a1bf381071b2e6635f
Brian Mak <eggrollbot@gmail.com>
2019-12-25 12:54:17 -0800

    Changed the EngineLevel to be a NWInt, instead of a server variable
    Changed the EngineLevel to be a NWInt, instead of a server variable.

--- lua/auto_body_npc/sv_hooks.lua
+++ lua/auto_body_npc/sv_hooks.lua
@@ -1,8 +1,8 @@
 hook.Add( "VC_CD_spawnedVehicle", "AutoBodyNPC_SpawnVehicle", function( ply, vehicle, test_drive ) -- We're going to interact as little as we can with VCMod because their API is terrible and most of the documentation sucks.
 	if not test_drive then
 		vehicle:SetNWEntity( "VCModOwner", ply )
-		-- set bodygroups, color, skin, underglow, and engine upgrades, if any. For engine upgrades, set vehicle.EngineLevel to 0-4. 0 being stock, 1-4 being EMS upgrades.
-		vehicle.EngineLevel = 0 -- Set it to 0 for now.
+		-- set bodygroups, color, skin, underglow, and engine upgrades, if any. For engine upgrades, set the vehicle's NWInt "EngineLevel" to 0-4. 0 being stock, 1-4 being EMS upgrades.
+		vehicle:SetNWInt( "EngineLevel", 0 ) -- Set it to 0 for now.
 	end
 end )
 
