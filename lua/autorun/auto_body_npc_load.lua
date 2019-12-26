2cfa09b43e15193393f6280f566342bf65790cac
Brian Mak <eggrollbot@gmail.com>
2019-12-25 23:27:49 -0800

    Fixed an extension mistake in the resource.AddFile for the garage
    Fixed an extension mistake in the resource.AddFile for the garage image. Changed the garage image's extension from png to jpg.

--- lua/autorun/auto_body_npc_load.lua
+++ lua/autorun/auto_body_npc_load.lua
@@ -8,7 +8,7 @@ if SERVER then
 	include( "auto_body_npc/sh_config.lua" )
 	include( "auto_body_npc/sv_hooks.lua" )
 	resource.AddFile( "resource/fonts/gratis.ttf" )
-	resource.AddFile( "materials/auto_body_npc/garage.png" )
+	resource.AddFile( "materials/auto_body_npc/garage.jpg" )
 	resource.AddFile( "materials/auto_body_npc/logo.png" )
 elseif CLIENT then
 	include( "auto_body_npc/cl_fonts.lua" )
