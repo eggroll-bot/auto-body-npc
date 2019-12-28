22b024be23a6e89aa04033e243b370efac2e6d7b
Brian Mak <eggrollbot@gmail.com>
2019-12-27 21:59:00 -0800

    Finished engine upgrade backend
    - Finished the engine upgrade backend.
    
    - Expanded on the to-do comment for underglow.

--- lua/auto_body_npc/sv_vehicles.lua
+++ lua/auto_body_npc/sv_vehicles.lua
@@ -2,10 +2,10 @@ local vehicle_meta = FindMetaTable( "Vehicle" )
 
 function vehicle_meta:SetEngineLevel( engine_level )
 	self:SetNWInt( "EngineLevel", engine_level )
-	-- apply engine modifications.
+	self:SetMaxThrottle( 1 + engine_level * 0.1 )
 end
 
 function vehicle_meta:SetUnderglowID( id )
 	self:SetNWInt( "UnderglowColor", id )
-	-- apply underglow.
+	-- apply underglow. find wheels and find point equidistant to them.
 end
