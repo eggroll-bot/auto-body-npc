62e0f9cc40ba5ad2d108db5f33c90719e77c3b87
Brian Mak <eggrollbot@gmail.com>
2019-12-27 18:13:52 -0800

    Finished data part of backend
    Finished data part of backend.

--- lua/auto_body_npc/sv_data.lua
+++ lua/auto_body_npc/sv_data.lua
@@ -31,45 +31,122 @@ Structure: JSON resulting from a table structure of the following:
 
 function vehicle_meta:SaveRespray( color )
 	local ply = self:GetNWEntity( "VCModOwner" )
-	local vcmod_id = vehicle.VC_CD_ID -- FFS, freemmaann. At least make your API work properly, so I don't have to do this.
+	local vcmod_id = self.VC_CD_ID -- FFS, freemmaann. At least make your API work properly, so I don't have to do this.
 
 	if not IsValid( ply ) or not vcmod_id then
 		return
+	end
+
+	local steam_id_64 = ply:SteamID64( )
+	local file_name = "auto_body_npc/ply_data/" .. steam_id_64 .. ".txt"
+
+	if not file.Exists( file_name, "DATA" ) then
+		local ply_vehicle_data = { }
+		ply_vehicle_data[ vcmod_id ] = { }
+		ply_vehicle_data[ vcmod_id ].Color = color
+		file.Write( file_name, util.TableToJSON( ply_vehicle_data ) )
+	else
+		local ply_vehicle_data = util.JSONToTable( file.Read( file_name ) )
+		ply_vehicle_data[ vcmod_id ] = ply_vehicle_data[ vcmod_id ] or { }
+		ply_vehicle_data[ vcmod_id ].Color = color
+		file.Write( file_name, util.TableToJSON( ply_vehicle_data ) )
 	end
 end
 
 function vehicle_meta:SaveSkin( skin )
 	local ply = self:GetNWEntity( "VCModOwner" )
-	local vcmod_id = vehicle.VC_CD_ID -- FFS, freemmaann. At least make your API work properly, so I don't have to do this.
+	local vcmod_id = self.VC_CD_ID -- FFS, freemmaann. At least make your API work properly, so I don't have to do this.
 
 	if not IsValid( ply ) or not vcmod_id then
 		return
+	end
+
+	local steam_id_64 = ply:SteamID64( )
+	local file_name = "auto_body_npc/ply_data/" .. steam_id_64 .. ".txt"
+
+	if not file.Exists( file_name, "DATA" ) then
+		local ply_vehicle_data = { }
+		ply_vehicle_data[ vcmod_id ] = { }
+		ply_vehicle_data[ vcmod_id ].Skin = skin
+		file.Write( file_name, util.TableToJSON( ply_vehicle_data ) )
+	else
+		local ply_vehicle_data = util.JSONToTable( file.Read( file_name ) )
+		ply_vehicle_data[ vcmod_id ] = ply_vehicle_data[ vcmod_id ] or { }
+		ply_vehicle_data[ vcmod_id ].Skin = skin
+		file.Write( file_name, util.TableToJSON( ply_vehicle_data ) )
 	end
 end
 
 function vehicle_meta:SaveBodygroup( bodygroup_id, bodygroup_value )
 	local ply = self:GetNWEntity( "VCModOwner" )
-	local vcmod_id = vehicle.VC_CD_ID -- FFS, freemmaann. At least make your API work properly, so I don't have to do this.
+	local vcmod_id = self.VC_CD_ID -- FFS, freemmaann. At least make your API work properly, so I don't have to do this.
 
 	if not IsValid( ply ) or not vcmod_id then
 		return
+	end
+
+	local steam_id_64 = ply:SteamID64( )
+	local file_name = "auto_body_npc/ply_data/" .. steam_id_64 .. ".txt"
+
+	if not file.Exists( file_name, "DATA" ) then
+		local ply_vehicle_data = { }
+		ply_vehicle_data[ vcmod_id ] = { }
+		ply_vehicle_data[ vcmod_id ].Bodygroups = ply_vehicle_data[ vcmod_id ].Bodygroups or { }
+		ply_vehicle_data[ vcmod_id ].Bodygroups[ bodygroup_id ] = bodygroup_value
+		file.Write( file_name, util.TableToJSON( ply_vehicle_data ) )
+	else
+		local ply_vehicle_data = util.JSONToTable( file.Read( file_name ) )
+		ply_vehicle_data[ vcmod_id ] = ply_vehicle_data[ vcmod_id ] or { }
+		ply_vehicle_data[ vcmod_id ].Bodygroups = ply_vehicle_data[ vcmod_id ].Bodygroups or { }
+		ply_vehicle_data[ vcmod_id ].Bodygroups[ bodygroup_id ] = bodygroup_value
+		file.Write( file_name, util.TableToJSON( ply_vehicle_data ) )
 	end
 end
 
 function vehicle_meta:SaveEngineLevel( engine_level )
 	local ply = self:GetNWEntity( "VCModOwner" )
-	local vcmod_id = vehicle.VC_CD_ID -- FFS, freemmaann. At least make your API work properly, so I don't have to do this.
+	local vcmod_id = self.VC_CD_ID -- FFS, freemmaann. At least make your API work properly, so I don't have to do this.
 
 	if not IsValid( ply ) or not vcmod_id then
 		return
+	end
+
+	local steam_id_64 = ply:SteamID64( )
+	local file_name = "auto_body_npc/ply_data/" .. steam_id_64 .. ".txt"
+
+	if not file.Exists( file_name, "DATA" ) then
+		local ply_vehicle_data = { }
+		ply_vehicle_data[ vcmod_id ] = { }
+		ply_vehicle_data[ vcmod_id ].EngineLevel = engine_level
+		file.Write( file_name, util.TableToJSON( ply_vehicle_data ) )
+	else
+		local ply_vehicle_data = util.JSONToTable( file.Read( file_name ) )
+		ply_vehicle_data[ vcmod_id ] = ply_vehicle_data[ vcmod_id ] or { }
+		ply_vehicle_data[ vcmod_id ].EngineLevel = engine_level
+		file.Write( file_name, util.TableToJSON( ply_vehicle_data ) )
 	end
 end
 
 function vehicle_meta:SaveUnderglowID( underglow_id )
 	local ply = self:GetNWEntity( "VCModOwner" )
-	local vcmod_id = vehicle.VC_CD_ID -- FFS, freemmaann. At least make your API work properly, so I don't have to do this.
+	local vcmod_id = self.VC_CD_ID -- FFS, freemmaann. At least make your API work properly, so I don't have to do this.
 
 	if not IsValid( ply ) or not vcmod_id then
 		return
+	end
+
+	local steam_id_64 = ply:SteamID64( )
+	local file_name = "auto_body_npc/ply_data/" .. steam_id_64 .. ".txt"
+
+	if not file.Exists( file_name, "DATA" ) then
+		local ply_vehicle_data = { }
+		ply_vehicle_data[ vcmod_id ] = { }
+		ply_vehicle_data[ vcmod_id ].UnderglowID = underglow_id
+		file.Write( file_name, util.TableToJSON( ply_vehicle_data ) )
+	else
+		local ply_vehicle_data = util.JSONToTable( file.Read( file_name ) )
+		ply_vehicle_data[ vcmod_id ] = ply_vehicle_data[ vcmod_id ] or { }
+		ply_vehicle_data[ vcmod_id ].UnderglowID = underglow_id
+		file.Write( file_name, util.TableToJSON( ply_vehicle_data ) )
 	end
 end
