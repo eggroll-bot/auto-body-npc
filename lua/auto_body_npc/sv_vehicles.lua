bfaf496696feb9af65ef49959bb86db05cf5f282
Brian Mak <eggrollbot@gmail.com>
2019-12-27 22:42:02 -0800

    Made the engine upgrade increase max health and health of the vehicle
    Made the engine upgrade increase the max health and health of the vehicle.

--- lua/auto_body_npc/sv_vehicles.lua
+++ lua/auto_body_npc/sv_vehicles.lua
@@ -3,6 +3,8 @@ local vehicle_meta = FindMetaTable( "Vehicle" )
 function vehicle_meta:SetEngineLevel( engine_level )
 	self:SetNWInt( "EngineLevel", engine_level )
 	self:SetMaxThrottle( 1 + engine_level * 0.1 )
+	self:VC_setHealthMax( self:VC_getHealthMax( ) * ( 1 + 0.2 * engine_level ) )
+	self:VC_setHealth( self:VC_getHealth( false ) * ( 1 + 0.2 * engine_level ) )
 end
 
 function vehicle_meta:SetUnderglowID( id )
