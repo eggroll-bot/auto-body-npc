ba71e45595ced87a17d5c6088e9e2c0427ef8592
Brian Mak <eggrollbot@gmail.com>
2020-01-02 15:40:17 -0800

    Created the vehicle preview DModelPanel and fixed an issue
    Created the vehicle preview DModelPanel and fixed an issue where the wrong variable was being set to nil.

--- lua/auto_body_npc/cl_panels.lua
+++ lua/auto_body_npc/cl_panels.lua
@@ -42,6 +42,7 @@ end
 vgui.Register( "AutoBodyNPCButton", PANEL, "DButton" )
 
 PANEL = { }
+local glow_material = Material( "sprites/light_ignorez" )
 
 function PANEL:GetTitle( )
 	return self.OriginalTitle
@@ -58,6 +59,77 @@ function PANEL:SetTitle( title )
 
 	self.OriginalTitle = string.upper( title )
 	self.Title = table.concat( exploded_title, " " )
+end
+
+function PANEL:SetVehiclePreviewModel( model )
+	local vehicle_modelpanel = self.VehiclePreview
+
+	if model then
+		vehicle_modelpanel:SetModel( model )
+		local preview_ent = vehicle_modelpanel:GetEntity( )
+		local min_bounds, max_bounds = preview_ent:GetRenderBounds( ) -- Re-set the cam position and cam angles.
+		local size = math.abs( min_bounds.x ) + math.abs( max_bounds.x )
+		size = math.max( size, math.abs( min_bounds.y ) + math.abs( max_bounds.y ) )
+		size = math.max( size, math.abs( min_bounds.z ) + math.abs( max_bounds.z ) )
+		vehicle_modelpanel:SetCamPos( Vector( size, size, size / 2 ) )
+		vehicle_modelpanel:SetLookAt( ( min_bounds + max_bounds ) / 2 )
+		local active_vehicle = self:GetActiveVehicle( )
+		vehicle_modelpanel:SetColor( active_vehicle:GetColor( ) )
+		preview_ent:SetSkin( active_vehicle:GetSkin( ) )
+
+		for _, v in pairs( active_vehicle:GetBodyGroups( ) ) do
+			local id = v.id
+			local value = active_vehicle:GetBodygroup( id )
+			preview_ent:SetBodygroup( id, value )
+		end
+
+		local underglow_id = self:GetActiveVehicle( ):GetNWInt( "UnderglowColor" )
+
+		if underglow_id ~= 0 then
+			local underglow_color = AutoBodyNPC.Config.GlobalUnderglowSettings[ underglow_id ].color
+
+			vehicle_modelpanel.PreDrawModel = function( )
+				render.SetMaterial( glow_material )
+				render.DrawSprite( preview_ent:GetPos( ), 256, 256, underglow_color ) -- Make it have a glowing sprite.
+
+				return true -- Default functionality
+			end
+		else -- If the previous vehicle had an underglow, clear it up.
+			vehicle_modelpanel.PreDrawModel = function( )
+				return true -- Default functionality
+			end
+		end
+
+		vehicle_modelpanel:Show( )
+	else
+		vehicle_modelpanel:Hide( )
+	end
+end
+
+function PANEL:UpdateVehiclePreview( upgrade_type, upgrade_value )
+	local vehicle_modelpanel = self.VehiclePreview
+	local preview_ent = vehicle_modelpanel:GetEntity( )
+
+	if upgrade_type == 1 then -- Respray -- Value: Color structure.
+		vehicle_modelpanel:SetColor( upgrade_value ) -- Using preview_ent.SetColor doesn't work for some reason.
+	elseif upgrade_type == 2 then -- Skin -- Value: Skin number.
+		preview_ent:SetSkin( upgrade_value )
+	elseif upgrade_type == 3 then -- Bodygroup -- Value: { bodygroup_id, bodygroup_value }.
+		preview_ent:SetBodygroup( upgrade_value[ 1 ], upgrade_value[ 2 ] )
+	elseif upgrade_type == 4 then -- Underglow -- Value: 0 = No underglow color, otherwise color structure provided.
+		if upgrade_value == 0 then
+			vehicle_modelpanel.PreDrawModel = function( )
+				return true -- Default functionality
+			end
+		else
+			vehicle_modelpanel.PreDrawModel = function( )
+				render.SetMaterial( glow_material )
+				render.DrawSprite( preview_ent:GetPos( ), 256, 256, upgrade_value ) -- Make it have a glowing sprite.
+
+				return true -- Default functionality
+			end
+		end
+	end
 end
 
 function PANEL:CreateBackground( )
@@ -79,6 +151,7 @@ function PANEL:CreateCloseButton( )
 
 	self.CloseButton.DoClick = function( )
 		self.Background:Remove( )
+		self.VehiclePreview:Remove( )
 		self:Remove( )
 	end
 end
@@ -116,6 +189,17 @@ function PANEL:CreateContentScrollPanel( )
 		surface.SetDrawColor( Color( 50, 50, 50 ) )
 		surface.DrawRect( 0, 0, w, h )
 	end
+end
+
+function PANEL:CreateVehiclePreview( )
+	self.VehiclePreview = vgui.Create( "DModelPanel" )
+	self.VehiclePreview:SetPos( ScrW( ) * 0.322, ScrH( ) * 0.3 )
+	self.VehiclePreview:SetSize( ScrW( ) * 0.5, ScrH( ) * 0.75 )
+	self.VehiclePreview:SetModel( "models/props_borealis/bluebarrel001.mdl" ) -- Temporary model. Just so it doesn't complain.
+	self.VehiclePreview:SetFOV( 45 )
+	self.VehiclePreview:SetCursor( "arrow" )
+	self.VehiclePreview.LayoutEntity = function( ) end
+	self.VehiclePreview:Hide( )
 end
 
 function PANEL:AddButton( text, do_click, bottom_spacing, right_aligned_text, large ) -- Adds a button using Panel.AddContent and docks it to the scroll panel.
@@ -181,6 +265,7 @@ function PANEL:Init( )
 	self:CreateTopBanner( )
 	self:CreateTitleBar( )
 	self:CreateContentScrollPanel( )
+	self:CreateVehiclePreview( )
 end
 
 function PANEL:Paint( w, h )
@@ -219,6 +304,7 @@ function PANEL:CreateResprayPanel( equipped )
 			net.WriteUInt( new_color.g, 8 )
 			net.WriteUInt( new_color.b, 8 )
 			net.SendToServer( )
+			self:UpdateVehiclePreview( 1, new_color ) -- 1 = Respray
 			local current_hoverlerp = btn.hoverlerp
 			self:CreateResprayPanel( new_color )
 			self.Elements[ 3 ].hoverlerp = current_hoverlerp -- Sets hoverlerp of the purchase button back to the original hoverlerp value.
@@ -252,6 +338,7 @@ function PANEL:CreateSkinsPanel( equipped ) -- Desync the equipped from the serv
 				net.WriteUInt( 2, 3 ) -- 2 = Skin.
 				net.WriteUInt( i, 8 )
 				net.SendToServer( )
+				self:UpdateVehiclePreview( 2, i ) -- 2 = Skin
 				local current_hoverlerp = btn.hoverlerp
 				self:CreateSkinsPanel( i )
 				self.Elements[ i + 2 ].hoverlerp = current_hoverlerp
@@ -293,6 +380,7 @@ function PANEL:CreateBodygroupPanel( bodygroup_id, name, pretty_name, num_option
 				net.WriteUInt( bodygroup_id, 8 )
 				net.WriteUInt( i, 8 )
 				net.SendToServer( )
+				self:UpdateVehiclePreview( 3, { bodygroup_id, i } ) -- 3 = Bodygroup
 				local current_hoverlerp = btn.hoverlerp
 				self:CreateBodygroupPanel( bodygroup_id, name, pretty_name, num_options, i )
 				self.Elements[ i + 2 ].hoverlerp = current_hoverlerp
@@ -388,8 +476,9 @@ function PANEL:CreateUnderglowPanel( equipped )
 			net.WriteEntity( self:GetNPC( ) )
 			net.WriteEntity( self:GetActiveVehicle( ) )
 			net.WriteUInt( 5, 3 ) -- 5 = Underglow.
-			net.WriteUInt( 0, 7 )
+			net.WriteUInt( 0, 7 ) -- 0 = No underglow color
 			net.SendToServer( )
+			self:UpdateVehiclePreview( 4, 0 ) -- 4 = Underglow, 0 = No underglow color
 			local current_hoverlerp = btn.hoverlerp
 			self:CreateUnderglowPanel( 0 )
 			self.Elements[ 2 ].hoverlerp = current_hoverlerp
@@ -412,6 +501,7 @@ function PANEL:CreateUnderglowPanel( equipped )
 				net.WriteUInt( 5, 3 ) -- 5 = Underglow.
 				net.WriteUInt( k, 7 )
 				net.SendToServer( )
+				self:UpdateVehiclePreview( 4, v.color ) -- 4 = Underglow
 				local current_hoverlerp = btn.hoverlerp
 				self:CreateUnderglowPanel( k )
 				self.Elements[ k + 2 ].hoverlerp = current_hoverlerp
@@ -455,7 +545,7 @@ end
 
 function PANEL:PopulateCars( )
 	self:Reset( )
-	self.ActiveCar = nil
+	self:SetActiveVehicle( nil )
 	self:SetTitle( "SELECT A CAR" )
 
 	for _, v in pairs( self:GetCarTable( ) ) do
@@ -490,6 +580,7 @@ end
 
 function PANEL:SetActiveVehicle( vehicle )
 	self.ActiveVehicle = vehicle
+	self:SetVehiclePreviewModel( vehicle and vehicle:GetModel( ) ) -- Argument will be nil if vehicle is nil.
 end
 
 vgui.Register( "AutoBodyNPCPanel", PANEL, "DPanel" )
